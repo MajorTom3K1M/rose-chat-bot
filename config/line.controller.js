8d67585fd28e3d074a19de5b42b7ca54c9bfdc8f
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-04-28 12:50:15 +0700

    add shopping list

--- config/line.controller.js
+++ config/line.controller.js
@@ -17,10 +17,61 @@ function handleEvent(event) {
 }
 
 function handleMessageEvent(event) {
+    var eventText = event.message.text.toLowerCase();
+
+    // Default Reply Message
     var msg = {
         type: 'text',
         text: 'Hi, Friend'
     };
+
+    if(eventText.include('รายการสินค้า')) {
+        msg = {
+            "type": "template",
+            "altText": "Shopping List",
+            "template": {
+                "type": "carousel",
+                "columns": [
+                    {
+                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
+                        "title": "this is menu",
+                        "text": "description",
+                        "actions": [
+                            {
+                                "type": "postback",
+                                "label": "Add to cart",
+                                "data": "action=add&itemid=111"
+                            },
+                            {
+                                "type": "uri",
+                                "label": "View detail",
+                                "uri": "http://example.com/page/111"
+                            }
+                        ]
+                    },
+                    {
+                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
+                        "title": "this is menu",
+                        "text": "description",
+                        "actions": [
+                            {
+                                "type": "postback",
+                                "label": "Add to cart",
+                                "data": "action=add&itemid=222"
+                            },
+                            {
+                                "type": "uri",
+                                "label": "View detail",
+                                "uri": "http://example.com/page/222"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+
+
     return client.replyMessage(event.replyToken, msg);
 }
 
