d61a7628245f660ecf5cc50236269a9af08f4453
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-04-28 12:15:30 +0700

    new 10

--- app.js
+++ app.js
@@ -13,6 +13,5 @@ app.use(cors())
 app.use('/apis/', router);
 
 app.get('/', (req, res) => res.send('Service Backend Start!'))
-app.get('/webhook', (req, res) => res.send("Hello"))
 
 app.listen(port, () => console.log(`Http app listening on port ${port}!`))
