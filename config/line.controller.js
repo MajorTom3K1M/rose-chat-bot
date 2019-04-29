dbcdd84f877a43a89bc1bd1dfbceba11061f4548
MajorTom3K1M <invaderzgame@gmail.com>
2019-04-29 22:30:27 +0700

    ADD Shop Menu

--- config/line.controller.js
+++ config/line.controller.js
@@ -17,10 +17,81 @@ function handleEvent(event) {
     }
 }
 function handleMessageEvent(event) {
+    var eventText = event.message.text.toLowerCase();
     var msg = {
         type: 'text',
-        text: 'Hi , Friend'
+        text: 'หนูไม่เข้าใจค่ะ'
     };
+
+    if(eventText.includes('รายการร้านค้า')) {
+        msg = {
+            "type": "template",
+            "altText": "this is a carousel template",
+            "template": {
+                "type": "carousel",
+                "columns": [
+                    {
+                      "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+                      "imageBackgroundColor": "#FFFFFF",
+                      "title": "this is menu",
+                      "text": "description",
+                      "defaultAction": {
+                          "type": "uri",
+                          "label": "View detail",
+                          "uri": "http://example.com/page/123"
+                      },
+                      "actions": [
+                          {
+                              "type": "postback",
+                              "label": "Buy",
+                              "data": "action=buy&itemid=111"
+                          },
+                          {
+                              "type": "postback",
+                              "label": "Add to cart",
+                              "data": "action=add&itemid=111"
+                          },
+                          {
+                              "type": "uri",
+                              "label": "View detail",
+                              "uri": "http://example.com/page/111"
+                          }
+                      ]
+                    },
+                    {
+                      "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+                      "imageBackgroundColor": "#000000",
+                      "title": "this is menu",
+                      "text": "description",
+                      "defaultAction": {
+                          "type": "uri",
+                          "label": "View detail",
+                          "uri": "http://example.com/page/222"
+                      },
+                      "actions": [
+                          {
+                              "type": "postback",
+                              "label": "Buy",
+                              "data": "action=buy&itemid=222"
+                          },
+                          {
+                              "type": "postback",
+                              "label": "Add to cart",
+                              "data": "action=add&itemid=222"
+                          },
+                          {
+                              "type": "uri",
+                              "label": "View detail",
+                              "uri": "http://example.com/page/222"
+                          }
+                      ]
+                    }
+                ],
+                "imageAspectRatio": "rectangle",
+                "imageSize": "cover"
+            }
+        }
+    }
     return client.replyMessage(event.replyToken, msg);
 }
 
