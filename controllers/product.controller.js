9e3d45d2f9d56eeca00a2d1c664abf08d71f7a43
MajorTom3K1M <invaderzgame@gmail.com>
2019-05-03 21:38:43 +0700

    ADD Catch err

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -90,7 +90,8 @@ const getProductById = async function (req, res) {
     let validation = new Validator(req.params, rules);
     validation.passes(async () => {
         await DB.collection('Products').doc(id).get()
-        .then((snapshot) =>  res.status(200).json({id:snapshot.id,...snapshot.data()}));
+        .then((snapshot) =>  res.status(200).json({id:snapshot.id,...snapshot.data()}))
+        .catch((err) => res.status(404).json({ id: 'Not Found' }));
     })
     validation.fails(() => {
         res.status(404).json(validation.errors)
