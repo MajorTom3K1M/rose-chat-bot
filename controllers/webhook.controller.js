ed5f775d7217a96c67a247838b8cfd82de2b919f
MajorTom3K1M <invaderzgame@gmail.com>
2019-05-03 23:34:28 +0700

    ADD catch

--- controllers/webhook.controller.js
+++ controllers/webhook.controller.js
@@ -3,7 +3,8 @@ const { handleEvent } = require('./line.controller')
 const WebHook = function (req, res) {
     Promise
         .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result));
+        .then((result) => res.json(result))
+        .catch(err => console.log(err));
 }
 
 module.exports = WebHook;
