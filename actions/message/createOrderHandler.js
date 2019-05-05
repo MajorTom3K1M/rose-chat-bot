7294f40cfd781c1f6d93888fbfb515b5d60970c9
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-05 07:40:22 +0700

    fix promise

--- actions/message/createOrderHandler.js
+++ actions/message/createOrderHandler.js
@@ -1,27 +1,27 @@
 const DB = require('../../config/firebase.config')
 
 module.exports = createOrderHandler = async event => {
+  let productsCollection = await DB.collection('Products').get()
   let columns = []
-  await DB.collection('Products').get()
-    .forEach(doc => {
-      columns.push({
-        thumbnailImageUrl: doc.data().picture,
-        title: doc.data().title,
-        text: "$" + doc.data().price + " (คงเหลือ " + doc.data().quantity + " ชิ้น)",
-        actions: [
-          {
-            type: "postback",
-            label: "Add to cart",
-            data: "action=order&itemid=" + doc.id + "&clientId=" + event.source.userId
-          },
-          {
-            type: "uri",
-            label: "View detail",
-            uri: "https://blackpinkmerch.com/"
-          }
-        ]
-      })
+  productsCollection.forEach(doc => {
+    columns.push({
+      thumbnailImageUrl: doc.data().picture,
+      title: doc.data().title,
+      text: "$" + doc.data().price + " (คงเหลือ " + doc.data().quantity + " ชิ้น)",
+      actions: [
+        {
+          type: "postback",
+          label: "Add to cart",
+          data: "action=order&itemid=" + doc.id + "&clientId=" + event.source.userId
+        },
+        {
+          type: "uri",
+          label: "View detail",
+          uri: "https://blackpinkmerch.com/"
+        }
+      ]
     })
+  })
 
   return msg = {
       type: "template",
