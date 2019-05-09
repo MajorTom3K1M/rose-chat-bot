23a1fbe19fd6b3a859cffcc40c5cd39788996c8e
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-10 06:04:05 +0700

    fix resolve order status

--- actions/util/createOrder.js
+++ actions/util/createOrder.js
@@ -3,14 +3,14 @@ const DB = require('../../config/firebase.config')
 module.exports = createOrder = async (clientId, items) => {
   await DB.collection('Orders').add({
     clientId: clientId,
-    items: items,
+    items: {items},
     status: "shopping"
   }).then((docRef) => {
       console.log("Document written with ID: ", docRef.id);
       let respone = { status: true }
       res.status(200).json(respone)
   }).catch((err) => {
-      console.error("Error adding document: ", error);
+      console.error("Error adding document: ", err);
       let respone = { status: false }
       res.status(200).json(respone)
   })
--- actions/util/resolveOrderStatus.js
+++ actions/util/resolveOrderStatus.js
@@ -1,14 +1,12 @@
 const DB = require('../../config/firebase.config')
 
 module.exports = resolveOrderStatus = async event => {
-  const orderSnapshot = await DB.collection('Orders');
-  let orderKey = orderSnapshot.get()
-                  .then(querySnapshot => {
-                      querySnapshot.docs
-                          .find(doc => {
-                              doc.data().clientId == event.source.userId && doc.data().status != "shipped" && doc.data().status != "cancelled"
-                          })
-                  })
-      
-  return orderKey.then(result => result === undefined) ? "None" : orderKey.then(result => result.data().status)
+  const order = await DB.collection('Orders')
+                        .where('clientId', '==', event.source.userId)
+                        .where('status', '!=', 'shipped')
+                        .where('status', '!=', 'cancelled')
+                        .get()
+
+  const status = order.data()[0].status
+  return status === undefined ? "None" : status
 }
