d64eee2c6f698682a6c57fb343d0bc74bdcdc353
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-10 06:16:41 +0700

    fix resolve order status

--- actions/util/resolveOrderStatus.js
+++ actions/util/resolveOrderStatus.js
@@ -3,8 +3,8 @@ const DB = require('../../config/firebase.config')
 module.exports = resolveOrderStatus = async event => {
   const order = await DB.collection('Orders')
                         .where('clientId', '==', event.source.userId)
-                        .where('status', '!=', 'shipped')
-                        .where('status', '!=', 'cancelled')
+                        .where('status', '<', 'shipped').where('status', '>', 'shipped')
+                        .where('status', '<', 'cancelled').where('status', '>', 'cancelled')
                         .get()
 
   const status = order.data()[0].status
