ac59ef9fd9465ba84ad0cec0ef0b7f225327cd24
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-11 00:12:27 +0700

    edit updateOrder

--- actions/util/getProductFromOrder.js
+++ actions/util/getProductFromOrder.js
@@ -1,18 +1,29 @@
 const DB = require('../../config/firebase.config')
 
-module.exports = async function getProductFromOrder(item, orderText) {
-  let orderText = "รายการสั่งซื้อ #" + order.id + "\n"
+module.exports = getProductFromOrder = async orderId => {
+  let orderText = "รายการสั่งซื้อ #" + orderId + "\n"
   let totalPrice = 0
-  let productPromise = await DB.collection('Product').get()
-                  .then(querySnapshot => {
-                    querySnapshot.docs
-                      .find(doc => doc.id == item.id)
-                        .data()
-                  })
 
-  productPromise.then(function(productPromise) {
-    totalPrice += parseFloat(productPromise.price) * parseInt(item.qty)
-    orderText += productPromise.title + " จำนวน " + item.qty + "\n"
+  let productCollection = await DB.collection('Product')
+                                  .get()
+                                  
+  let productList = productCollection.docs
+  let productMap = new Map()
+  productList.forEach(product => {
+    productMap.set(key=product.id, product.data().price)
+  })
+  let productNameMap = new Map()
+  productList.forEach(product => {
+    productNameMap.set(key=product.id, product.data().title)
+  })
+
+  let order = await DB.collection('Orders')
+                      .doc(orderId)
+                      .get()
+  let orderItems = order.data().items
+  orderItems.forEach(item => {
+    totalPrice += parseFloat(productMap.get(item.itemId)) * parseInt(item.qty)
+    orderText += productNameMap.get(item.itemId) + " จำนวน " + item.qty + "\n"
   })
 
   return orderText += "ราคารวมทั้งหมด $" + totalPrice
