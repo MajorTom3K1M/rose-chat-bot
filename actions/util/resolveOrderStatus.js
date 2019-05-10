51ff270ee16bbc0dd935b13e5bb1e8f22405d148
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-10 13:48:33 +0700

    fix resolve order status

--- actions/util/resolveOrderStatus.js
+++ actions/util/resolveOrderStatus.js
@@ -1,18 +1,34 @@
 const DB = require('../../config/firebase.config')
 
 module.exports = resolveOrderStatus = async event => {
-  const orderSnapshot = await DB.collection('Orders')
-                                .where('clientId', '==', event.source.userId)
-                                .where('status', '<', 'shipped').where('status', '>', 'shipped')
-                                .where('status', '<', 'cancelled').where('status', '>', 'cancelled')
-  
-  let status = 'None'
-  orderSnapshot.get()
-               .then(snapshot => {
-                 let docs = snapshot.docs
-                 status = docs.find(doc => doc.get('status') === 'shopping' || doc.get('status') === 'paying')
-                              .get('status')
-               })
-  
+  let allOrders = []
+  await DB.collection('Orders')
+          .where('clientId', '==', event.source.userId)
+          .where('status', '==', 'shopping')
+          .onSnapshot(snapshot => {
+            snapshot.forEach(doc => {
+              allOrders.push(doc.data().status)
+            })
+          })
+
+  await DB.collection('Orders')
+          .where('clientId', '==', event.source.userId)
+          .where('status', '==', 'cancelled')
+          .onSnapshot(snapshot => {
+            snapshot.forEach(doc => {
+              allOrders.push(doc.data().status)
+            })
+          })
+          
+  let status = allOrders.length <= 0 ? 'None' : allOrders.pop()
   return status
+  
+  /*
+  console.log(orderSnapshot)
+
+  let orderDocs = orderSnapshot.docs
+  if (orderDocs.length <= 0) return "None"                      
+  let currentOrderStatus = orderDocs[0].get('status')
+  return currentOrderStatus
+  */
 }
