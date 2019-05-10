fd285d3b3589a476cc598c9db2893b011d49d7d1
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-11 04:59:05 +0700

    fix shopping list

--- actions/util/showShoppingList.js
+++ actions/util/showShoppingList.js
@@ -1,74 +1,113 @@
 const DB = require('../../config/firebase.config')
 
-module.exports = showShoppingList = async event => {
-	let productsCollection = await DB.collection('Products').get()
-	let contentList = []
-	productsCollection.forEach(doc => {
-		contentList.push({
-			type: 'bubble',
-			hero: {
-				type: 'image',
-				url: doc.data().picture,
-				size: 'full',
-				aspectMode: 'cover',
-				aspectRatio: '20:13'
-			},
-			body: {
-				type: 'box',
-				layout: 'vertical',
-				contents: [
-					{
-						type: 'text',
-						text: doc.data().title,
-						wrap: true,
-						weight: 'bold',
-						size: 'lg'
-					},
-					{
-						type: 'text',
-						text: '$' + doc.data().price,
-						size: 'xl'
-					},
-					{
-						type: 'text',
-						text: 'เหลือ ' + doc.data().quantity + ' ชิ้น'
-					},
-					{
-						type: 'text',
-						text:
-							parseInt(doc.data().quantity) > 0
-								? ''
-								: 'สินค้าชิ้นนี้หมดชั่วคราวนะคะ TT',
-						color: '#ff5551',
-						size: 'xs'
-					}
-				]
-			},
-			footer: {
-				type: 'box',
-				layout: 'vertical',
-				spacing: 'md',
-				contents: [
-					{
-						type: 'button',
-						style: parseInt(doc.data().quantity) > 0 ? 'primary' : 'secondary',
-						action: {
-							type: 'postback',
-							label:
-								parseInt(doc.data().quantity) > 0 ? 'ใส่ตะกร้า' : 'สินค้าหมด',
-							data:
-								parseInt(doc.data().quantity) > 0
-									? '?action=manageorder&items=' +
-									  doc.id +
-									  '&clientId=' +
-									  event.source.userId
-									: ''
-						}
-					}
-				]
-			}
-		});
-	});
+module.exports = showShoppingList = async (event) => {
+  let productsCollection = await DB.collection('Products').get()
+  let contentList = []
+  productsCollection.forEach(doc => {
+    if(parseInt(doc.data().quantity) > 0) {
+      contentList.push({
+        type: 'bubble',
+        hero: {
+          type: 'image',
+          url: doc.data().picture,
+          size: 'full',
+          aspectMode: 'cover',
+          aspectRatio: '20:13'
+        },
+        body: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+              type: 'text',
+              text: doc.data().title,
+              wrap: true,
+              weight: 'bold',
+              size: 'lg'
+            },
+            {
+              type: 'text',
+              text: '$' + doc.data().price,
+              size: 'xl'
+            },
+            {
+              type: 'text',
+              text: 'ยอดสินค้าคงเหลือ ' + doc.data().quantity + ' ชิ้น'
+            }
+          ]
+        },
+        footer: {
+          type: 'box',
+          layout: 'vertical',
+          spacing: 'md',
+          contents: [
+            {
+              type: 'button',
+              style: 'primary',
+              action: {
+                type: 'postback',
+                label: 'ใส่ตะกร้า',
+                data: '?action=manageorder&items=' + doc.id + '&clientId=' + event.source.userId
+              }
+            }
+          ]
+        }     
+      })
+    }
+    else {
+      contentList.push({
+        type: 'bubble',
+        hero: {
+          type: 'image',
+          url: doc.data().picture,
+          size: 'full',
+          aspectMode: 'cover',
+          aspectRatio: '20:13'
+        },
+        body: {
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+              type: 'text',
+              text: doc.data().title,
+              wrap: true,
+              weight: 'bold',
+              size: 'lg'
+            },
+            {
+              type: 'text',
+              text: '$' + doc.data().price,
+              size: 'xl'
+            },
+            {
+              type: 'text',
+              text: 'หมดแล้วค่ะ T^T',
+              color: '#ff5551',
+              size: 'xs'
+            }
+          ]
+        },
+        footer: {
+          type: 'box',
+          layout: 'vertical',
+          spacing: 'md',
+          contents: [
+            {
+              type: 'button',
+              style: 'secondary',
+              action: {
+                type: 'uri',
+                label: 'ใส่ตะกร้า',
+                uri: 'https://blackpinkmerch.com/'
+              }
+            }
+          ]
+        }     
+      })
+    }
+    
+  })
 
-	return contentList;
-};
+  return contentList
+}
