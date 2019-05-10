655d80b8c84fbede25fe15ac43dd175e62a336fa
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-10 22:28:17 +0700

    fix updateOrder

--- actions/util/updateOrder.js
+++ actions/util/updateOrder.js
@@ -1,25 +1,27 @@
 const DB = require('../../config/firebase.config')
+const admin = require('firebase-admin');
 
-module.exports = updateOrder = async (clientId, itemId) => {
-  let order = await DB.collection('Orders')
-                      .where("clientId","==",clientId)
-                      .where("status","==","shopping")
-                      .get()
-  order.forEach(async doc => {
-    const index = doc.data()
-                     .items
-                     .findIndex((item) => itemId == item.id);
-    const item = doc.data()
-                    .items[index];
-
-    await DB.collection('Orders')
-            .doc(doc.id)
-            .update({
-              items: [
-                ...doc.data().items.slice(0, index),
-                { ...item, qty: Number(item.qty) + 1 },
-                ...doc.data().items.slice(index + 1)
-              ]
-            })
+module.exports = updateOrder = async (event, itemId) => {
+  let userOrderCollection = await DB.collection('Orders')
+                                .where('clientId', '==', event.source.userId)
+                                .get()
+  let userOrderDocs = userOrderCollection.docs
+  userOrderDocs.forEach(order => {
+    let userStatus = order.get('status')
+    if(userStatus != 'cancelled' && userStatus != 'shipped') {
+      order.forEach(async doc => {
+          let index = doc.data().items.findIndex(item => itemId === item.itemId)
+          let item = doc.data().items[index]
+          await DB.collection('Orders')
+                  .doc(doc.id)
+                  .update({items: [
+                    ...doc.data().items.slice(0, index),
+                    { ...item, 
+                      qty: Number(item.qty) + 1
+                    },
+                    ...doc.data().items.slice(index + 1)
+                ]})
+      })
+    }
   })
 }
