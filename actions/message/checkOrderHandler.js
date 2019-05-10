91eefbb11fc9d8c1dfe564bbb4bbc000928aae4e
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-11 00:17:55 +0700

    edit updateOrder

--- actions/message/checkOrderHandler.js
+++ actions/message/checkOrderHandler.js
@@ -1,13 +1,8 @@
-const resolveOrderSnapshot = require('../util/resolveOrderByStatus')
 const getProductFromOrder = require('../util/getProductFromOrder')
 
-module.export = checkOrderHandler = event => {
-  let orderPromise = resolveOrderSnapshot(event, 'shopping')
-    .then(result => result.data().items)
-    .then(forEach(item => getProductFromOrder(item)))
-
+module.export = checkOrderHandler = async event => {
   return msg = {
       type: 'text',
-      text: orderPromise.then(value => value)
+      text: await getProductFromOrder(event)
   }
 }
