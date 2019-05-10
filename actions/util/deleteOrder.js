9b5a59e7f5ecbc659aa18497b2fd08350efbe357
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-10 21:30:59 +0700

    add deleteOrder

---
+++ actions/util/deleteOrder.js
@@ -0,0 +1,18 @@
+const DB = require('../../config/firebase.config')
+
+module.exports = createOrder = async (clientId, items) => {
+  await DB.collection('Orders').add({
+    clientId: clientId,
+    items: [
+      {
+        itemId: items,
+        qty: 1
+      }
+    ],
+    status: "shopping"
+  }).then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+  }).catch((err) => {
+      console.error("Error adding document: ", err);
+  })
+}
--- actions/util/updateOrder.js
+++ actions/util/updateOrder.js
@@ -4,16 +4,13 @@ module.exports = updateOrder = async (clientId, itemId) => {
   let order = await DB.collection('Orders')
                       .where("clientId","==",clientId)
                       .where("status","==","shopping")
-                      .get();
+                      .get()
   order.forEach(async doc => {
     const index = doc.data()
                      .items
                      .findIndex((item) => itemId == item.id);
     const item = doc.data()
                     .items[index];
-
-    console.log(index, "  ", item);
-    console.log(doc.id);
 
     await DB.collection('Orders')
             .doc(doc.id)
