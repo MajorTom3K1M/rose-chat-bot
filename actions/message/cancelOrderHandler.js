020b233f8fd9689f12ec86d52f93b16014f6374a
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-11 05:13:33 +0700

    add some wording

--- actions/message/cancelOrderHandler.js
+++ actions/message/cancelOrderHandler.js
@@ -2,8 +2,15 @@ const deleteOrder = require('../util/deleteOrder')
 
 module.exports = cancelOrderHandler = async event => {
   await deleteOrder(event)
-  return msg = {
+  return msg = [
+    {
       type: 'text',
-      text: 'ยกเลิกรายการปัจจุบัน'
-  }
+      text: 'ยกเลิกรายการปัจจุบันแล้วนะเจ้าคะ'
+    },
+    {
+      type: 'sticker',
+      packageId: '11537',
+      stickerId: '52002740'
+    }
+  ]
 }
