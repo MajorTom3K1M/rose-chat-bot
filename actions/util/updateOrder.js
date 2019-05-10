5a5f3a73b0c1b7935a9d0d8ca9a0539a93d2252f
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-10 23:25:01 +0700

    edit updateOrder

--- actions/util/updateOrder.js
+++ actions/util/updateOrder.js
@@ -9,17 +9,18 @@ module.exports = updateOrder = async (clientId, itemId) => {
     let userStatus = order.get('status')
     if(userStatus != 'cancelled' && userStatus != 'shipped') {
       order.forEach(async doc => {
-          let index = doc.data().items.findIndex(item => itemId === item.itemId)
-          let item = doc.data().items[index]
-          await DB.collection('Orders')
-                  .doc(doc.id)
-                  .update({items: [
-                    ...doc.data().items.slice(0, index),
-                    { ...item, 
-                      qty: Number(item.qty) + 1
-                    },
-                    ...doc.data().items.slice(index + 1)
-                ]})
+        let index = doc.data().items.findIndex(item => itemId === item.itemId)
+        let item = doc.data().items[index]
+        await DB.collection('Orders')
+                .doc(doc.id)
+                .update({items: [
+                          ...doc.data().items.slice(0, index),
+                          { 
+                            ...item, 
+                            qty: Number(item.qty) + 1
+                          },
+                          ...doc.data().items.slice(index + 1)
+                        ]})
       })
     }
   })
