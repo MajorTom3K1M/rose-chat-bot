ac723c17005adabe20ac16966fc1a5803c4ec4b3
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-10 19:39:26 +0700

    fix fire store issue

--- actions/debug/debugHandler.js
+++ actions/debug/debugHandler.js
@@ -1,10 +1,8 @@
 const resolveOrderStatus = require('../util/resolveOrderStatus')
 
 module.exports = debugHandler = async event => {
-  resolveOrderStatus(event, status).then(event, status => {
-    return msg = {
-      type: 'text',
-      text: status 
-    }
-  })
+  return msg = {
+    type: 'text',
+    text: resolveOrderStatus(event)
+  }
 }
