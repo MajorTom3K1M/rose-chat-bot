c6b49581103203376edf77feaafd379bbd74b23a
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-11 04:27:08 +0700

    try sticker

--- actions/util/showShoppingList.js
+++ actions/util/showShoppingList.js
@@ -4,61 +4,110 @@ module.exports = showShoppingList = async (event) => {
   let productsCollection = await DB.collection('Products').get()
   let contentList = []
   productsCollection.forEach(doc => {
-    contentList.push({
-      type: 'bubble',
-      header: {
-        type: 'box',
-        layout: 'vertical',
-        contents: [
-          {
-            type: 'text',
-            text: doc.data().title,
-            wrap: true,
-            weight: 'bold'
-          }
-        ]
-      },
-      hero: {
-        type: 'image',
-        url: doc.data().picture,
-        size: 'full',
-        aspectMode: 'cover'
-      },
-      body: {
-        type: 'box',
-        layout: 'horizontal',
-        contents: [
-          {
-            type: 'text',
-            text: '$' + doc.data().price,
-            size: 'lg'
-          },
-          {
-            type: 'separator'
-          },
-          {
-            type: 'text',
-            text: 'เหลือ ' + doc.data().quantity + ' ชิ้น'
-          }
-        ]
-      },
-      footer: {
-        type: 'box',
-        layout: 'vertical',
-        spacing: 'md',
-        contents: [
-          {
-            type: 'button',
-            style: 'primary',
-            action: {
-              type: 'postback',
-              label: 'ใส่ตะกร้า',
-              data: '?action=manageorder&items=' + doc.id + '&clientId=' + event.source.userId
+    if(doc.data().quantity > 0) {
+      contentList.push({
+        type: 'bubble',
+        hero: {
+          type: 'image',
+          url: doc.data().picture,
+          size: 'full',
+          aspectMode: 'cover'
+        },
+        body: {
+          type: 'box',
+          layout: 'horizontal',
+          contents: [
+            {
+              type: 'text',
+              text: doc.data().title,
+              wrap: true,
+              weight: 'bold',
+              size: 'lg'
+            },
+            {
+              type: 'text',
+              text: '$' + doc.data().price,
+              size: 'xl'
+            },
+            {
+              type: 'separator'
+            },
+            {
+              type: 'text',
+              text: 'เหลือ ' + doc.data().quantity + ' ชิ้น'
             }
-          }
-        ]
-      }     
-    })
+          ]
+        },
+        footer: {
+          type: 'box',
+          layout: 'vertical',
+          spacing: 'md',
+          contents: [
+            {
+              type: 'button',
+              style: 'primary',
+              action: {
+                type: 'postback',
+                label: 'ใส่ตะกร้า',
+                data: '?action=manageorder&items=' + doc.id + '&clientId=' + event.source.userId
+              }
+            }
+          ]
+        }     
+      })
+    }
+    else {
+      contentList.push({
+        type: 'bubble',
+        hero: {
+          type: 'image',
+          url: doc.data().picture,
+          size: 'full',
+          aspectMode: 'cover'
+        },
+        body: {
+          type: 'box',
+          layout: 'horizontal',
+          contents: [
+            {
+              type: 'text',
+              text: doc.data().title,
+              wrap: true,
+              weight: 'bold',
+              size: 'lg'
+            },
+            {
+              type: 'text',
+              text: '$' + doc.data().price,
+              size: 'xl'
+            },
+            {
+              type: 'text',
+              text: 'หมดแล้วค่ะ T^T',
+              color: '#ff5551',
+              size: 'xs'
+            }
+          ]
+        },
+        footer: {
+          type: 'box',
+          layout: 'vertical',
+          spacing: 'md',
+          contents: [
+            {
+              type: 'button',
+              style: 'secondary',
+              action: {
+                type: 'uri',
+                label: 'ใส่ตะกร้า',
+                uri: ''
+              }
+            }
+          ]
+        }     
+      })
+    }
+    
   })
 
   return contentList
