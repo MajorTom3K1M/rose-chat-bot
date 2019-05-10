c865df4122c789d293382d266cd93ce10bb3948c
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-11 06:39:01 +0700

    fix symbol falut

--- actions/message/showHistoryHandler.js
+++ actions/message/showHistoryHandler.js
@@ -25,7 +25,7 @@ module.exports = showHistoryHandler = async event => {
       body: {
         type: 'box',
         layout: 'vertical',
-        contents = msg
+        contents: msg
       }
     }
   }
