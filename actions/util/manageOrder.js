a74b406be6577eb4055137da1fd60b039ab51046
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-10 23:20:51 +0700

    add manageOrder

--- actions/util/manageOrder.js
+++ actions/util/manageOrder.js
@@ -5,9 +5,11 @@ const updateOrder = require('./updateOrder')
 module.exports = manageOrder = async (clientId, items) => {
   let status = await resolveOrderStatus(clientId)
   if(status === 'None') {
+    console.log('create')
     await createOrder(clientId, items)
   }
   else {
+    console.log('update')
     await updateOrder(clientId, items)
   }
 }
