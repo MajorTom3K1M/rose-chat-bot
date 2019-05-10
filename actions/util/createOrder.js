122b6fd9833388a6b43fe1d93e892e943fe778ce
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-11 04:35:38 +0700

    fix shopping list

--- actions/util/createOrder.js
+++ actions/util/createOrder.js
@@ -1,18 +1,25 @@
 const DB = require('../../config/firebase.config')
 
 module.exports = createOrder = async (clientId, items) => {
-  await DB.collection('Orders').add({
-    clientId: clientId,
-    items: [
-      {
-        itemId: items,
-        qty: 1
-      }
-    ],
-    status: "shopping"
-  }).then((docRef) => {
-      console.log("Document written with ID: ", docRef.id);
-  }).catch((err) => {
-      console.error("Error adding document: ", err);
-  })
+  let productCollection = await DB.collection('Products')
+                                  .where('itemId', '==', 'items')
+                                  .get()
+
+  let productQuantity = productCollection.docs.pop().get('quantity')
+  if(productQuantity > 0) {
+    await DB.collection('Orders').add({
+      clientId: clientId,
+      items: [
+        {
+          itemId: items,
+          qty: 1
+        }
+      ],
+      status: "shopping"
+    }).then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    }).catch((err) => {
+        console.error("Error adding document: ", err);
+    })
+  }
 }
--- actions/util/updateOrder.js
+++ actions/util/updateOrder.js
@@ -2,6 +2,12 @@ const DB = require('../../config/firebase.config')
 var admin = require('firebase-admin');
 
 module.exports = updateOrder = async (clientId, itemId) => {
+  let productCollection = await DB.collection('Products')
+                                  .where('itemId', '==', 'items')
+                                  .get()
+
+  let productQuantity = productCollection.docs.pop().get('quantity')
+
   let userOrderCollection = await DB.collection('Orders')
                                 .where('clientId', '==', clientId)
                                 .get()
@@ -13,6 +19,9 @@ module.exports = updateOrder = async (clientId, itemId) => {
       let isUpdated = false
       for(i = 0; i < itemList.length; i++) {
         if(itemList[i].itemId === itemId) {
+          if(productQuantity - itemList[i].qty < 0) {
+            return Promise.resolve(null)
+          }
           itemList[i].qty += 1
           isUpdated = true
         }
@@ -21,6 +30,9 @@ module.exports = updateOrder = async (clientId, itemId) => {
         order.ref.update({ items: itemList })
       }
       else {
+        if(productQuantity - 1 < 0) {
+          return Promise.resolve(null)
+        }
         order.ref.update({
           items: admin.firestore.FieldValue.arrayUnion({
             itemId: itemId,
