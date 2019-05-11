27e85b51ea8e21faa7d1ef6c6a603834c7ebd1b6
Yuttapichai Kerdcharoen <yuttapichai.ke@gmail.com>
2019-05-11 07:01:58 +0700

    fix math

--- actions/util/checkoutOrder.js
+++ actions/util/checkoutOrder.js
@@ -2,17 +2,20 @@ const DB = require('../../config/firebase.config')
 
 module.exports = checkoutOrder = async event => {
   let userOrderCollection = await DB.collection('Orders')
-                                .where('clientId', '==', event.source.userId)
-                                .get()
+                                    .where('clientId', '==', event.source.userId)
+                                    .get()
+
   let userOrderDocs = userOrderCollection.docs
   userOrderDocs.forEach(async order => {
     let userStatus = order.get('status')
     if(userStatus != 'cancelled' && userStatus != 'shipped') {
       let items = order.get('items')
       for(i = 0; i < items.length; i++) {
+        let oldQty = await DB.collection('Products').doc(items[0].itemId).get('quantity')
+        let newQty = parseInt(oldQty) - parseInt(items[0].qty)
         await DB.collection('Products')
                 .doc(items[0].itemId)
-                .update({quantity: DB.FieldValue.increment(-1)})
+                .update({quantity: newQty})
       }
       await DB.collection('Orders')
               .doc(order.id)
