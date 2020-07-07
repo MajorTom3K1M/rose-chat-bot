4c6090c0cc9d8846aa74705a20d16d675aac20ee
Jakkarin <invaderzgame@gmail.com>
2020-07-08 02:22:12 +0700

    make it better

--- config/line.config.js
+++ config/line.config.js
@@ -1,4 +1,4 @@
 module.exports = configLine = {
-    channelAccessToken: process.env.channelAccessToken,
-    channelSecret: process.env.channelSecret
+    channelAccessToken: "94gII6XNUQI4zrvWhcF0ZpWaSVbckYrVwMItD+u8KJqiQWVCf3or1Itnw7smwD3sRT/RxYhTsF64FNbNf3hdpjjPFrxHP/ia3EIuTnBxbrN3p503dvurKBLENrUmoQKs5AS455BaJifJjq34ZEPsvwdB04t89/1O/w1cDnyilFU=",
+    channelSecret: "c627a11f0a43dea2f10b83bf0f0682e1"
 }
