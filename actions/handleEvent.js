56784eb11379a2b8f5df2b2f16222644a6d375d5
Jakkarin <invaderzgame@gmail.com>
2020-07-08 17:10:14 +0700

    make it better

--- actions/handleEvent.js
+++ actions/handleEvent.js
@@ -1,5 +1,6 @@
 const handleMessageEvent = require('./event/messageEventHandler')
 const followEventHandler = require('./event/followEventHandler')
+const joinEventHandler = require('./event/joinEventHandler')
 const postbackEventHandler = require('./event/postbackEventHandler')
 
 module.exports = handleEvent = event => {
@@ -9,9 +10,9 @@ module.exports = handleEvent = event => {
   switch(event.type) {
     case 'message': handleMessageEvent(event)
       break
-    case 'follow': followEventHandler(event)
+    case 'follow': joinEventHandler(event)
       break
-    case 'join': followEventHandler(event)
+    case 'join': joinEventHandler(event)
       break
     case 'postback': postbackEventHandler(event)
       break
